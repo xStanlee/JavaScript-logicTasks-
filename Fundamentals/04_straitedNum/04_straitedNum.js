4e13e4481127eb06982be1d399c5d53ca9f02902
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-09-14 18:04:12 +0200

    Right update
    Added CustomErr. by extending property from Error class and reduce cost of catch memory usage.

--- Fundamentals/04_straitedNum/04_straitedNum.js
+++ Fundamentals/04_straitedNum/04_straitedNum.js
@@ -24,37 +24,67 @@
 * spełniają tego warunku, funkcja powinna rzucić wyjątek.
 */
 
-function zipIt(first, second = 0) {
-  let arrResult = [];
-  const arrOne = Array.from(first.toString());
-  const arrTwo = Array.from(second.toString());
-  const lenghtOfLongerArray = 
-        (arrOne.length >= arrTwo.length) ? arrOne.length : arrTwo.length;
-  for (let i = 0; i < lenghtOfLongerArray; i++){
-    if((arrOne[i] !== 'undefined') && (arrTwo[i] !== 'undefined')){
-      arrResult.push(arrOne[i]);
-      arrResult.push(arrTwo[i]);
-    }else if (arrOne[i] === 'undefined'){
-      arrResult.push(arrTwo[i]);
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 3 - "Przeplatanie"
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję, która dla dwóch podanych parametrów wykona "przeplatanie"
+* - używając kolejnych cyfr parametrów połączy je w jeden string.
+*
+* Przykład:
+* zipIt(111, 222) // => '121212'
+* zipIt(123, 456) // => '142536'
+* zipIt(12, 5555) // => '152555'
+*/
+
+/*
+* Punkty dodatkowe
+*-----------------
+* Funkcja powinna weryfikować, czy przekazane parametry są typu number. Jeśli parametry nie
+* spełniają tego warunku, funkcja powinna rzucić wyjątek.
+*/
+
+class CustomError extends Error{
+  constructor(message){
+    super(message);
+    this.message = message;
+  };
+
+  notANum() {
+    return console.log('Put an Number inside zipIt boi');
+  };
+}
+
+function zipIt(first, second) {
+  if (isNaN(first) || isNaN(second)){
+    throw new CustomError('One of passed arguments is not a Number Type.');
+  }
+  first = Array.from(first.toString());
+  second = Array.from(second.toString());
+  const properLength = first.length >= second.length ? first.length : second.length;
+  let properZip = '';
+  
+  for (let i = 0; i < properLength; i++){
+    if(typeof first[i] !== 'undefined' && typeof second[i] !== 'undefined'){
+      properZip += first[i];
+      properZip += second[i];
+    }else if(typeof first[i] === 'undefined'){
+      properZip += second[i];
     }else{
-      arrResult.push(arrOne[i]);
+      properZip += first[i];
     }
   }
-  return arrResult;
+  
+  return properZip;
 }
 
-function arrToNumber(arr){
-  let striatedNum = '';
-  arr.map(el => {
-    striatedNum+=el;
-  })
-  return striatedNum;
-}
-
-console.log(arrToNumber(zipIt(1111, 2345)));
-
-
-// Weryfikacja 
+ //Weryfikacja 
 
 function verify(input, goal) {
   if (input === goal) {
@@ -64,6 +94,11 @@ function verify(input, goal) {
   }
 }
 
-verify(arrToNumber(zipIt(111, 222)), '121212');
-verify(arrToNumber(zipIt(123, 456)), '142536');
-verify(arrToNumber(zipIt(12, 5555)), '152555');
+try {
+  verify(zipIt(111, 222), '121212');
+  verify(zipIt(123, 456), '142536');
+  verify(zipIt(12, 555), '152555'); 
+}catch(e){
+  e.notANum();
+  console.log(e.message);
+}
