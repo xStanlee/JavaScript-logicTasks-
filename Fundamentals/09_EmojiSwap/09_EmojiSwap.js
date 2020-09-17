59545dc5d8d45d50360f2b867f92faac026ec0c0
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-09-17 09:36:44 +0200

    Using passed object swap emoji in text()
    Goal as in description. Using passed object. You've to replace words to emoji in passed sentence.

---
+++ Fundamentals/09_EmojiSwap/09_EmojiSwap.js
@@ -0,0 +1,49 @@
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 7 - "Emoji"
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję "useEmoji", która zastąpi wszystkie słowa danego parametru zgodnie 
+* z mapowaniem z obiektu "emojiMappings";
+*
+* Przykład:
+* useEmoji('Takiemu zachowaniu mówię stop i to mocny stop!') // => 'Takiemu zachowaniu mówię 🚫 i to mocny 🚫!'
+* useEmoji('Jadę po nowy samochód :D') // => 'Jadę po nowy 🏎 :D'
+*/
+
+const emojiMappings = {
+  'stop': '🚫',
+  'gwiazda': '⭐️',
+  'samochód': '🏎',
+  'buduję': '🧱',
+  'budzik': '⏰'
+};
+const entriesEmoji = Object.entries(emojiMappings);
+
+function useEmoji(input) {
+  let changedString = '';
+  for (const [key, value] of entriesEmoji){
+    if (input.includes(key)){
+      let regex = new RegExp(key, 'g');
+      changedString = input.replace(regex, value);
+    }
+  }
+  return changedString;
+}
+/* Weryfikacja */
+function verify(input, goal) {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
+
+verify(useEmoji('Takiemu zachowaniu mówię stop i to mocny stop!'), 'Takiemu zachowaniu mówię 🚫 i to mocny 🚫!');
+verify(useEmoji('Jadę po nowy samochód :D'), 'Jadę po nowy 🏎 :D');
+verify(useEmoji('Właśnie buduję swoje skille w JS'), 'Właśnie 🧱 swoje skille w JS');
