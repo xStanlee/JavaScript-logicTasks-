627daa80903ebc43a1df3bd4abdf349eacfe5b5e
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-10-12 19:46:30 +0200

    Functions for Cookies way

---
+++ Browser/codingChallenge_03/functions.js
@@ -0,0 +1,11 @@
+// Function for cookies explanation of this task
+
+function setCookie(val){
+  document.cookie=`value=${val}`;
+}
+
+function removeShure(info){
+  info.parentElement.removeChild(info);
+}
+
+export {}/*removeShure, setCookie}*/
