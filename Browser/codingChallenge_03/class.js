0cb6c904594feaa2f2a368ee74078ccc14fa801c
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-10-12 19:47:12 +0200

    Class for local/sessionStorage way

---
+++ Browser/codingChallenge_03/class.js
@@ -0,0 +1,17 @@
+class AppStorage {
+  
+  constructor() {
+    this.storage = localStorage
+  }
+
+  set(key, value) {
+    this.storage.setItem(key, JSON.stringify(value));
+  }
+
+  get(key) {
+    const data = this.storage.getItem(key);
+    return JSON.parse(data);
+  }
+}
+
+export {AppStorage};
