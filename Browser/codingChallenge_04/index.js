a379254de9354ebd434c8a75a238f9380fbb499a
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-10-14 22:52:51 +0200

    Update index.js

--- Browser/codingChallenge_04/index.js
+++ Browser/codingChallenge_04/index.js
@@ -1,39 +1,95 @@
 // Import stylesheets
 import "./style.css";
 
-const articleContainer = document.querySelector("main");
-const arrOfArticles = [...articleContainer.children];
-console.log(articleContainer);
-/////////////////////////////////////////
-///////////// DRAG
-let x = 0;
-let y = 0;
-let isHolding = false;
-let articleNode = {};
-
-arrOfArticles.forEach(el => {
-  el.addEventListener("mousedown", e => {
-    articleNode = el.cloneNode(true);
-    isHolding = true;
-    setTimeout(() => {
-      el.style.display = "none";
-    }, 1000);
-  });
+let draggedElement;
+const wrappers = document.querySelectorAll("main");
+wrappers.forEach(main => {
+  main.setAttribute("class", "wrapper");
 });
 
-///////////////////////////////////////////
-/////////////// DROP
-window.document.addEventListener("mouseup", e => {
-  if (isHolding === true) {
-    x = e.clientX || 800;
-    y = e.clinetY || 400;
-    let whereToWrapper = document.elementFromPoint(x, y);
-    if (whereToWrapper.children.length === 0) {
-      whereToWrapper.appendChild(articleNode);
-    } else {
-      whereToWrapper = whereToWrapper.parentElement;
-      whereToWrapper.appendChild(articleNode);
+function addPropertiesToArtciles(nodeList) {
+  let idForArtcile = 0;
+  nodeList.forEach(main => {
+    const article = main.children;
+    for (const every of article) {
+      setSpecifiedAttributes(every, idForArtcile);
+      idForArtcile++;
     }
-    isHolding = false;
+  });
+}
+
+function setSpecifiedAttributes(article, id) {
+  article.setAttribute("draggable", "true");
+  article.setAttribute("id", `article__${id}`);
+}
+
+addPropertiesToArtciles(wrappers);
+
+document.addEventListener("drag", () => {}, false);
+document.addEventListener(
+  "dragstart",
+  ev => {
+    draggedElement = ev.target;
+    if (
+      ev.target.tagName === "header".toUpperCase() ||
+      ev.target.tagName === "footer".toUpperCase() ||
+      ev.target.tagName === "a".toUpperCase() ||
+      ev.target.tagName === "img".toUpperCase() || // oneParentElement
+      ev.target.tagName === "p".toUpperCase() ||
+      ev.target.tagName === "h1".toUpperCase() // Make func from it
+    ) {
+      draggedElement = ev.target.parentElement;
+      draggedElement.tagName === "article".toUpperCase()
+        ? draggedElement
+        : (draggedElement = draggedElement.parentElement.parentElement);
+    }
+    draggedElement.style.opacity = ".6";
+  },
+  false
+);
+
+document.addEventListener(
+  "dragend",
+  ev => {
+    ev.target.style.opacity = "";
+  },
+  false
+);
+
+document.addEventListener(
+  "dragover",
+  ev => {
+    ev.preventDefault();
+  },
+  false
+);
+
+document.addEventListener("dragenter", ev => {
+  if (ev.target.className == "wrapper") {
+    ev.target.style.background = "purple";
   }
 });
+
+document.addEventListener(
+  "dragleave",
+  ev => {
+    if (ev.target.className === "wrapper") {
+      ev.target.style.background = "";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "drop",
+  e => {
+    e.preventDefault();
+
+    if (e.target.className === "wrapper") {
+      e.target.style.background = "";
+      draggedElement.parentNode.removeChild(draggedElement);
+      e.target.appendChild(draggedElement);
+    }
+  },
+  false
+);
