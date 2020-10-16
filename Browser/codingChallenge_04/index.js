250322ccf96124b12214ffd40fbb949661793b8c
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-10-16 11:58:01 +0200

    Wymeczony

--- Browser/codingChallenge_04/index.js
+++ Browser/codingChallenge_04/index.js
@@ -3,8 +3,20 @@ import "./style.css";
 
 let draggedElement;
 const wrappers = document.querySelectorAll("main");
+let wrapperID = 0;
+addPropertiesToArtciles(wrappers);
+
 wrappers.forEach(main => {
-  main.setAttribute("class", "wrapper");
+  main.setAttribute("id", `wrapper__${wrapperID}`);
+  main.addEventListener("dragover", ev => {
+    ev.preventDefault();
+  });
+  main.addEventListener("drop", ev => {
+    const droppingData = ev.dataTransfer.getData("text");
+    const article = document.querySelector(droppingData);
+    main.appendChild(article);
+  });
+  wrapperID++;
 });
 
 function addPropertiesToArtciles(nodeList) {
@@ -13,83 +25,21 @@ function addPropertiesToArtciles(nodeList) {
     const article = main.children;
     for (const every of article) {
       setSpecifiedAttributes(every, idForArtcile);
+      addDraggedElementListener(idForArtcile);
       idForArtcile++;
     }
   });
-}
 
-function setSpecifiedAttributes(article, id) {
-  article.setAttribute("draggable", "true");
-  article.setAttribute("id", `article__${id}`);
-}
-
-addPropertiesToArtciles(wrappers);
-
-document.addEventListener("drag", () => {}, false);
-document.addEventListener(
-  "dragstart",
-  ev => {
-    draggedElement = ev.target;
-    if (
-      ev.target.tagName === "header".toUpperCase() ||
-      ev.target.tagName === "footer".toUpperCase() ||
-      ev.target.tagName === "a".toUpperCase() ||
-      ev.target.tagName === "img".toUpperCase() || // oneParentElement
-      ev.target.tagName === "p".toUpperCase() ||
-      ev.target.tagName === "h1".toUpperCase() // Make func from it
-    ) {
-      draggedElement = ev.target.parentElement;
-      draggedElement.tagName === "article".toUpperCase()
-        ? draggedElement
-        : (draggedElement = draggedElement.parentElement.parentElement);
-    }
-    draggedElement.style.opacity = ".6";
-  },
-  false
-);
-
-document.addEventListener(
-  "dragend",
-  ev => {
-    ev.target.style.opacity = "";
-  },
-  false
-);
-
-document.addEventListener(
-  "dragover",
-  ev => {
-    ev.preventDefault();
-  },
-  false
-);
-
-document.addEventListener("dragenter", ev => {
-  if (ev.target.className == "wrapper") {
-    ev.target.style.background = "purple";
+  function setSpecifiedAttributes(article, id) {
+    article.setAttribute("draggable", "true");
+    article.setAttribute("id", `article__${id}`);
   }
-});
 
-document.addEventListener(
-  "dragleave",
-  ev => {
-    if (ev.target.className === "wrapper") {
-      ev.target.style.background = "";
-    }
-  },
-  false
-);
-
-document.addEventListener(
-  "drop",
-  e => {
-    e.preventDefault();
-
-    if (e.target.className === "wrapper") {
-      e.target.style.background = "";
-      draggedElement.parentNode.removeChild(draggedElement);
-      e.target.appendChild(draggedElement);
-    }
-  },
-  false
-);
+  function addDraggedElementListener(id) {
+    const articleID = `#article__${id}`;
+    const el = document.querySelector(articleID);
+    el.addEventListener("dragstart", ev => {
+      ev.dataTransfer.setData("text/plain", articleID);
+    });
+  }
+}
