d1f88f10ac506e15a3895a8951dd8cc5173981c3
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-10-27 15:00:10 +0100

    Upgraded class

--- Fundamentals/08_Calculate-time/exports/Majster.js
+++ Fundamentals/08_Calculate-time/exports/Majster.js
@@ -1,27 +1,40 @@
 class Majster{
+
+  _fullTime = 44264;
+
+  set fullTime(val) {
+    this._fullTime = val;
+  }
+  
   constructor(name, seconds, minutes = 0){
     this.name = name;
-    this.minutes = minutes;
     this.seconds = seconds;
+    this.minutes = minutes;
 
     this.hours = 0;
+    this.percentage = 0;
   }
 
-  calcMinutes(){
+  calcPercentage() {
+    this.percentage = Math.round((this.seconds / this._fullTime) * 100) + '%';
+  }
+
+  calcMinutes() {
+      this.calcPercentage();
       let restOfSeconds = this.seconds % 60;
       this.seconds -= restOfSeconds;
       this.minutes += Math.round(this.seconds/60);
       this.seconds = restOfSeconds;
     }
   
-  calcHours(){
+  calcHours() {
     let restOfMinutes = this.minutes % 60;
     this.minutes -= restOfMinutes;
     this.hours = Math.round(this.minutes / 60);
     this.minutes = restOfMinutes; 
   }
 
-  goodOrNo(){
+  goodOrNo() {
     return console.log(`${this.name}: Not bad?`);
   }
 }
