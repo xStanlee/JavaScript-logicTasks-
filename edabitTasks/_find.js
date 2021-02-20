3221c0e93c7080a3c6c174aee37781542d4f9d40
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2021-02-20 21:42:56 +0100

    updated only callback func() implementation left

--- edabitTasks/_find.js
+++ edabitTasks/_find.js
@@ -1,17 +1,36 @@
-function myFind(pass, req) {
+export function myFind(pass, req) {
     let obj = pass;
+
     for (const elem of obj) {
-        if(Object.values(elem).includes(...Object.values(req))){
+        if (typeof req != "object" &&
+                   Object.keys(elem).includes(req) &&
+                   elem[req] != false) {
+            // passed value equal to string
             return elem;
         }
-        /*
-            for string passed, but cannot be like this, cuz of B(o) notation
 
-            for(const prop in elem) {
-            if(elem[prop] === Boolean(req)){
-                return elem;
-            };
+        if(Object.entries(elem).flat().includes(req[0]) && Object.entries(elem).flat().includes(req[1])) {
+            // passed value equal to array
+            return elem;
+        }
 
-        }*/
+        if(Object.values(elem).includes(...Object.values(req))) {
+            // passed value equal to object
+            return elem;
+        }
     }
 }
+
+
+var users = [
+    { "user": "barney",  "age": 36, "active": true },
+    { "user": "fred",    "age": 40, "active": false },
+    { "user": "pebbles", "age": 1,  "active": true }
+  ]
+
+  // test for string
+  myFind(users, "active");
+  // test for array
+  myFind(users, ["active", false]);
+  // test for object
+  myFind(users, { "age": 1, "active": true });
