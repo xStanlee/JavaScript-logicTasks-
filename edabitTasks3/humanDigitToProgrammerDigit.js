8455db7d3f71c7646ab19921243ce5284cf76f6c
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2021-02-27 22:09:12 +0100

    Create humanDigitToProgrammerDigit.js

---
+++ edabitTasks3/humanDigitToProgrammerDigit.js
@@ -0,0 +1,10 @@
+replaceNums("I have 2 sheep.");
+
+replaceNums("My father was born in 1974.10.25.");
+
+replaceNums("10hell76o4 boi");
+
+function replacesNums(str) {
+  const arrBaseForChange = Array.from(str);
+  return arrBaseForChange;
+}
